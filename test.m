93c17cbae6fbe49af48e4cdf4073a14d9cb8d566
sergiocamara <sergio.camara@alu.uclm.es>
2020-04-22 13:25:19 +0200

    Nuevos cambios en la generatión de entrenamiento y  los datos

--- test.m
+++ test.m
@@ -1,51 +1,57 @@
 
 [output,name_ccaa,iso_ccaa, data_spain] = HistoricDataSpain()
 
-y = reshape(output.historic{1}.DailyCases,1,[]);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%% PROCESAMIENTO DE DATOS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%dataTrain=smooth(y,0.1,'moving')';
-%dataTrainaux=smooth(y,0.1,'moving')';
-dataTrainaux=y;
-dataTrain=y;
-mu = mean(dataTrain);
-sig = std(dataTrain);
-dataTrainStandardized = (dataTrain - mu) / sig;
-XTrain = dataTrainStandardized(1:end-1);
-YTrain = dataTrainStandardized(2:end);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%% DEFINICION DE RED
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-numFeatures = 1;
-numResponses = 1;
-numHiddenUnits = 100;
-layers = [ ...
-    sequenceInputLayer(numFeatures)
-    lstmLayer(numHiddenUnits)
-    fullyConnectedLayer(numResponses)
-    regressionLayer];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%% OPCIONES
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-options = trainingOptions('adam', ...
-    'MaxEpochs',100, ...
-    'GradientThreshold',1, ...
-    'InitialLearnRate',0.005, ...
-    'LearnRateSchedule','piecewise', ...
-    'LearnRateDropPeriod',5, ...
-    'LearnRateDropFactor',0.05, ...
-    'Verbose',0);%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%% ENTRENAMIENTO
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-net = trainNetwork(XTrain,YTrain,layers,options);
+  
+y = reshape(output.historic{1}.DailyCases,[],1)
+T = tonndata(y,false,false);
+% Create a Nonlinear Autoregressive Network
+feedbackDelays = 1:2;
+hiddenLayerSize = 2;
+net = narnet(feedbackDelays,hiddenLayerSize);
+% Prepare the Data for Training and Simulation
+% The function PREPARETS prepares timeseries data for a particular network,
+% shifting time by the minimum amount to fill input states and layer states.
+% Using PREPARETS allows you to keep your original time series data unchanged, while
+% easily customizing it for networks with differing numbers of delays, with
+% open loop or closed loop feedback modes.
+[x,xi,ai,t] = preparets(net,{},{},T);
+% Setup Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 85/100;
+net.divideParam.valRatio = 5/100;
+net.divideParam.testRatio = 10/100;
+% Train the Network
+[net,tr] = train(net,x,t,xi,ai);
+% Test the Network
+y = net(x,xi,ai);
+e = gsubtract(t,y);
+performance = perform(net,t,y)
+% View the Network
+view(net)
+% Plots
+% Uncomment these lines to enable various plots.
+%figure, plotperform(tr)
+%figure, plottrainstate(tr)
+figure, plotresponse(t,y)
+%figure, ploterrcorr(e)
+%figure, plotinerrcorr(x,e)
+% Closed Loop Network
+% Use this network to do multi-step prediction.
+% The function CLOSELOOP replaces the feedback input with a direct
+% connection from the outout layer.
+netc = closeloop(net);
+[xc,xic,aic,tc] = preparets(netc,{},{},T);
+yc = netc(xc,xic,aic);
+perfc = perform(net,tc,yc)
+% Step-Ahead Prediction Network
+% For some applications it helps to get the prediction a timestep early.
+% The original network returns predicted y(t+1) at the same time it is given y(t+1).
+% For some applications such as decision making, it would help to have predicted
+% y(t+1) once y(t) is available, but before the actual y(t+1) occurs.
+% The network can be made to return its output a timestep early by removing one delay
+% so that its minimal tap delay is now 0 instead of 1.  The new network returns the
+% same outputs as the original network, but outputs are shifted left one timestep.
+nets = removedelay(net);
+[xs,xis,ais,ts] = preparets(nets,{},{},T);
+ys = nets(xs,xis,ais);
+stepAheadPerformance = perform(net,ts,ys)
 
-
- targetSeries = tonndata(y,false,false);
-   
-    netc = closeloop(net);
-   
-     [Xc,Xic,Aic,Tc] = preparets(netc,{},{},targetSeries);
-    % NOW it's getting interesting:
-    outputc = netc(Xc,Xic,Aic);
